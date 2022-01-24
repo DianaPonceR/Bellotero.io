b12d869ac55d444db0dfefb329e8cf163dcda7af
Diana Ponce <dianagpr17@gmail.com>
2022-01-24 17:44:11 -0600

    Config Inicial - Estructura incial de un proyecto react con webpack funcionanado. - Falta configurar correctamente sass

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Hallo Welt!</h1>
+            <p>Bellotero app</p>
+        </>
+    );
+};
+
+export default Home;
